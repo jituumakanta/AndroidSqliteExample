4aa1b8e0605b2555fedcb7020d9716c8ce1a0b11
jituumakanta <jitu.umakanta@gmail.com>
2017-01-17 15:16:34 +0530

    databasehandeler java codes added

--- app/src/main/java/com/example/volmopc1/androidsqliteexample/Contact.java
+++ app/src/main/java/com/example/volmopc1/androidsqliteexample/Contact.java
@@ -8,38 +8,42 @@ public class Contact {
     int _id;
     String _name;
     String _phone_number;
-    public Contact(){   }
-    public Contact(int id, String name, String _phone_number){
+
+    public Contact() {
+    }
+
+    public Contact(int id, String name, String _phone_number) {
         this._id = id;
         this._name = name;
         this._phone_number = _phone_number;
     }
 
-    public Contact(String name, String _phone_number){
+    public Contact(String name, String _phone_number) {
         this._name = name;
         this._phone_number = _phone_number;
     }
-    public int getID(){
+
+    public int getID() {
         return this._id;
     }
 
-    public void setID(int id){
+    public void setID(int id) {
         this._id = id;
     }
 
-    public String getName(){
+    public String getName() {
         return this._name;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this._name = name;
     }
 
-    public String getPhoneNumber(){
+    public String getPhoneNumber() {
         return this._phone_number;
     }
 
-    public void setPhoneNumber(String phone_number){
+    public void setPhoneNumber(String phone_number) {
         this._phone_number = phone_number;
     }
 }
--- app/src/main/java/com/example/volmopc1/androidsqliteexample/DatabaseHandler.java
+++ app/src/main/java/com/example/volmopc1/androidsqliteexample/DatabaseHandler.java
@@ -2,8 +2,12 @@ package com.example.volmopc1.androidsqliteexample;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jitu on 1/17/2017.
@@ -47,5 +51,75 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         db.insert(TABLE_CONTACTS, null, values);
         //2nd argument is String containing nullColumnHack
         db.close(); // Closing database connection
+    }
+
+    // code to update the single contact
+    public int updateContact(Contact contact) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, contact.getName());
+        values.put(KEY_PH_NO, contact.getPhoneNumber());
+
+        // updating row
+        return db.update(TABLE_CONTACTS, values, KEY_ID + " = ?", new String[] { String.valueOf(contact.getID()) });
+    }
+
+    // Deleting single contact
+    public void deleteContact(Contact contact) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_CONTACTS, KEY_ID + " = ?", new String[] { String.valueOf(contact.getID()) });
+        db.close();
+    }
+
+
+
+    // code to get all contacts in a list view
+    public List<Contact> getAllContacts() {
+        List<Contact> contactList = new ArrayList<Contact>();
+        // Select All Query
+        String selectQuery = "SELECT  * FROM " + TABLE_CONTACTS;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                Contact contact = new Contact();
+                contact.setID(Integer.parseInt(cursor.getString(0)));
+                contact.setName(cursor.getString(1));
+                contact.setPhoneNumber(cursor.getString(2));
+                // Adding contact to list
+                contactList.add(contact);
+            } while (cursor.moveToNext());
+        }
+
+        // return contact list
+        return contactList;
+    }
+    // Getting contacts Count
+    public int getContactsCount() {
+        String countQuery = "SELECT  * FROM " + TABLE_CONTACTS;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(countQuery, null);
+        cursor.close();
+
+        // return count
+        return cursor.getCount();
+    }
+
+    // code to get the single contact
+    Contact getContact(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_CONTACTS, new String[] { KEY_ID, KEY_NAME, KEY_PH_NO }, KEY_ID + "=?", new String[] { String.valueOf(id) }, null, null, null, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Contact contact = new Contact(Integer.parseInt(cursor.getString(0)),
+                cursor.getString(1), cursor.getString(2));
+        // return contact
+        return contact;
     }
 }
